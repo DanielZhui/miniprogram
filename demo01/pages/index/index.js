a7c315af4d9950df5e0435f7385ddf3e1f0757e6
Wollens <zh2017728@163.com>
2020-02-01 21:24:07 +0800

    miniprogram global config

--- demo01/pages/index/index.js
+++ demo01/pages/index/index.js
@@ -1,66 +1,29 @@
-// pages/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    name: "Tom",
+    city: "Tokyo",
+    persons: [
+      { name: "Alice", age: 9 },
+      { name: "Alex", age: 10 },
+      { name: "Jerry", age: 10 }
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // Page 中绑定方法
+  handleBtnClick(){
+    console.log(this.data.counter)
+    // 不能够直接改变counter值,需要通过setData方法改变Data 中的值
+    this.setData({
+      counter: this.data.counter += 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter -= 1
+    })
   }
 })
