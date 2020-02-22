56169a7d3387b65f8cd58fa164e9d1c6bec16f39
Wollens <zh2017728@163.com>
2020-02-22 14:56:45 +0800

    add custom components

---
+++ demo01/components/customCmp/title.js
@@ -0,0 +1,24 @@
+// components/customCmp/title.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    title: "this is custom components title",
+    content: "welcome to components"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
